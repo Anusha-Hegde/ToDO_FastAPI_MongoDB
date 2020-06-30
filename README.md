1bbaefe08d32259bc6322112259f4cf4d3751357
Anusha-Hegde <anushaprhegde@gmail.com>
2020-06-30 10:25:25 +0530

    front end implementation

--- README.md
+++ README.md
@@ -15,24 +15,31 @@ How to run:
 - open mogoDB and create db `toDo`
 - clone the repository
 - compile toDo.py
-- open: 127.0.0.3/5000/docs
+- open: 127.0.0.3/5000/
 
 
 Screenshots:
 
-1. Inserting ToDo
+
+1. {Front end} Insert
+![ ](Screenshots/front_end_insert.png)
+
+1. {Front end} Update
+![ ](Screenshots/front_end_update.png)
+
+3. Inserting ToDo
 ![ ](Screenshots/insert.png)
 
 database reflection
 ![ ](Screenshots/insertDB.png)
 
-2. Update toDo
+4. Update toDo
 ![ ](Screenshots/update.png)
 
 database reflection
 ![ ](Screenshots/updateDB.png)
 
-3. Delete toDo
+5. Delete toDo
 ![ ](Screenshots/delete.png)
 
 database reflection
--- toDo.py
+++ toDo.py
@@ -2,43 +2,146 @@ import uvicorn
 from fastapi import FastAPI, Request, Depends, BackgroundTasks, WebSocket
 from fastapi.responses import HTMLResponse
 import motor.motor_asyncio
+from typing import Optional
 
 
 app = FastAPI()
+
+
+
+
+html = """
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>ToDo</title>
+    </head>
+    <body>
+        <h1>Insert your Todo</h1>
+        <input type="text" id="name" autocomplete="off" placeholder="enter name"/>
+        <input type="text" id="desc" autocomplete="off" placeholder="enter desc"/>
+        <button onclick="addToDo()">Create</button>
+        <button onclick="deleteToDo()">Delete</button>
+        <button onclick="update()">Update</button>
+		<p>
+		<ul>
+			<li>create: enter new name and description of toDo and click create,</li>
+			<li>update: click on update button to edit toDo,</li>
+			<li>delete: enter name and description of toDo and click delete,</li> 	
+		</ul>
+		</p>
+        <script>
+        	function addToDo() {
+        		name = document.getElementById("name").value
+            	desc = document.getElementById("desc").value
+				window.location = "http://127.0.0.3:5000/insert/" + name + "/" + desc;
+            }
+			function update(){
+				window.location = "http://127.0.0.3:5000/update"
+			}
+			function updateToDo() {
+        		name = document.getElementById("name").value
+            	desc = document.getElementById("desc").value
+				window.location = "http://127.0.0.3:5000/update/" + name + "/" + desc;
+            }
+			function deleteToDo() {
+        		name = document.getElementById("name").value
+            	desc = document.getElementById("desc").value
+				window.location = "http://127.0.0.3:5000/delete/" + name + "/" + desc;
+            }
+                name.value = ''
+                desc.value = ''
+        </script>
+    </body>
+</html>
+
+"""
+
+
+html2 = """
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>ToDo</title>
+    </head>
+    <body>
+        <h1>Insert your Todo</h1>
+        <input type="text" id="old_name" autocomplete="off" placeholder="enter old_name"/>
+        <input type="text" id="old_desc" autocomplete="off" placeholder="enter old_desc"/>
+		<br>
+        <input type="text" id="new_name" autocomplete="off" placeholder="enter new_name"/>
+        <input type="text" id="new_desc" autocomplete="off" placeholder="enter new_desc"/>
+		<br>
+		<button onclick="updateToDo()">Update</button>
+		<p>
+		click on update after entering name of toDo (description if needed)
+		</p>
+        <script>
+			function updateToDo() {
+        		old_name = document.getElementById("old_name").value
+            	old_desc = document.getElementById("old_desc").value
+				new_name = document.getElementById("new_name").value
+            	new_desc = document.getElementById("new_desc").value
+				window.location = "http://127.0.0.3:5000/update/" + old_name + "/" + old_desc + "/" + new_name + "/" + new_desc;
+            }
+        </script>
+    </body>
+</html>
+
+"""
 	
 
-@app.get("/insert")
+@app.get("/update")
+async def getUpdate():
+    return HTMLResponse(html2)
+
+
+
+
+@app.get("/")
+async def get():
+    return HTMLResponse(html)
+
+
+
+
+@app.get("/insert/{name}/{desc}")
 async def insert_item(name, desc):
 	client = motor.motor_asyncio.AsyncIOMotorClient('localhost', 27017)
 	db = client.toDo
 	document = {'name': name, 'desc': desc}
 	result = await db.toDo_collec.insert_one(document)
 	print('result %s' % repr(result.inserted_id))
+	return HTMLResponse(html)
 
 
    
-@app.put("/update")
-async def update_item(name = None, new_desc = None):
+@app.get("/update/{old_name}/{old_desc}/{new_name}/{new_desc}")
+async def update_item(old_name, old_desc, new_name, new_desc):
 	client = motor.motor_asyncio.AsyncIOMotorClient('localhost', 27017)
 	db = client.toDo
 	
 	coll = db.toDo_collec
-	#doc = await coll.find_one({'name': old_name})
+	doc = await coll.find_one({'name': old_name})
 	
-	#name = new_name if new_name is not None else old_name
-	#desc = new_desc if new_desc is not None else old_desc
-		
-	result = await coll.update_one({'name': name}, {'$set': {'desc': new_desc}})
+	if new_name is not None: name = new_name
+	else: name = old_name
+	if new_desc is not None: desc = new_desc
+	else: 
+		if old_desc is not None: desc = old_desc
+		else: desc = doc['desc']
+ 		
+	result = await coll.update_one({'_id': doc['_id']}, {'$set': {'name': name, 'desc': desc}})
+	return HTMLResponse(html)
+
     
-    
-@app.put("/delete")
-async def delete_item(name):
+@app.get("/delete/{name}/{desc}")
+async def delete_item(name, desc):
 
 	client = motor.motor_asyncio.AsyncIOMotorClient('localhost', 27017)
 	db = client.toDo
-	
-	result = await db.toDo_collec.delete_many({'name': name})
-
+	result = await db.toDo_collec.delete_many({'name': name, 'desc' : desc})
+	return HTMLResponse(html)
 
 if __name__ == "__main__":
     uvicorn.run("toDo:app", host="127.0.0.3", port=5000, log_level="info")
