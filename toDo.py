b43d977720510c40d9b8188979a670859bf021f2
Anusha-Hegde <anushaprhegde@gmail.com>
2020-07-01 15:36:32 +0530

    websockets for login/signup

--- toDo.py
+++ toDo.py
@@ -1,6 +1,6 @@
 import uvicorn, secrets
 import database
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, WebSocket
 from fastapi.templating import Jinja2Templates
 
 
@@ -13,34 +13,52 @@ app = FastAPI()
 
 @app.get("/")
 def authenticate(request : Request):
-	return templates.TemplateResponse("login_signup.html", {
+	return templates.TemplateResponse("signIn.html", {
 		"request" : request
 	})
 
 
-@app.get("/signIn/{name}/{password}")
-async def signIn(request : Request, name, password):
-	coll = database.getUser_collec()
-	doc = await coll.find_one({'name': name})
-	if password == doc['password']: return templates.TemplateResponse("layout1.html", {
+@app.get("/signup")
+async def signUp(request : Request):
+	print("h]ellooo")
+	return templates.TemplateResponse("signUp.html", {
 		"request" : request
 	})
-	else: return templates.TemplateResponse("incorrect_login.html", {
-		"request" : request
-	})
-    
 
 
-@app.get("/signUp/{name}/{password}")
-async def signUp(request : Request, name : str, password : str):
-	coll = database.getUser_collec()
-	document = {'name': name, 'password': password}
-	doc = await coll.find_one({'name': name})
-	result = await coll.insert_one(document)
+
+@app.get("/insert")
+async def insert_page(request : Request):
 	return templates.TemplateResponse("layout1.html", {
 		"request" : request
 	})
+
+
+@app.get("/wrong_login")
+async def disp(request : Request):
+	return templates.TemplateResponse("incorrect_login.html", {
+		"request" : request
+	})
 	
+
+@app.websocket("/signIn")
+async def websocket_endpoint(websocket: WebSocket):
+    await websocket.accept()
+    while True:
+        data = await websocket.receive_json()
+        coll = database.getUser_collec()
+        result = await coll.find_one(data)
+        if result is None: await websocket.send_text("wrong credentials")
+        else: await websocket.send_text("wright credentials")
+
+@app.websocket("/signUp")
+async def websocket_endpoint(websocket: WebSocket):
+    await websocket.accept()
+    while True:
+        data = await websocket.receive_json()
+        coll = database.getUser_collec()
+        result = await coll.insert_one(data)
+        await websocket.send_text("Sign up successful")
 
 
 @app.get("/insert/{name}/{desc}")
@@ -92,4 +110,4 @@ async def delete_item(request : Request, name, desc):
 	})
 
 if __name__ == "__main__":
-    uvicorn.run("toDo:app", host="127.0.0.4", port=5000, log_level="info")
+    uvicorn.run("toDo:app", host="127.0.0.1", port=5000, log_level="info")
